93e2434c28133905e43a46d92cc6755c802de65c
Vn-ChemGio <vn.chemgio@yahoo.com>
2023-02-07 00:55:13 +0700

    Init empty project with NestJS and FastifyAdapter

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import helmet from '@fastify/helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.register(helmet);
+
+  await app.listen(3000, '0.0.0.0');
+}
+
+bootstrap();
